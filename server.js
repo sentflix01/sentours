4f107944e7ec87ce525a803586201a8110720437
Sintayehu <sentflix@gmail.com>
2025-05-02 10:23:59 +0300

    lf

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const dotenv = require('dotenv');
+const app = require('./app');
 
 dotenv.config({ path: './config.env' });
-const app = require('./app');
 const port = process.env.port || 3000;
 app.listen(port, () => {
   console.log(`App running on port ${port}...`);
