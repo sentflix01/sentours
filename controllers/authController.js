3ddd96c68d07e0f6a92b7144f409e1de3528440b
Sintayehu <sentflix@gmail.com>
2025-07-09 12:08:54 +0300

    authentication

--- controllers/authController.js
+++ controllers/authController.js
@@ -15,6 +15,18 @@ const signToken = (id) =>
 
 const createSendToken = (user, statusCode, res) => {
   const token = signToken(user._id);
+  const cookieOptions = {
+    expires: new Date(
+      Date.now() +
+        Number(process.env.JWT_COOKIE_EXPIRE_IN) * 24 * 60 * 60 * 1000,
+    ),
+    httpOnly: true,
+  };
+  if (process.env.NODE_ENV === 'production') cookieOptions.secure = true;
+  res.cookie('jwt', token, cookieOptions);
+
+  // Remove the password the output
+  user.password = undefined;
 
   res.status(statusCode).json({
     status: 'success',
