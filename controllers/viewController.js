93ca8cdb156bd80800d5386f4727623e1ec2829d
Sintayehu <sentflix@gmail.com>
2025-07-31 00:12:23 +0300

    009 Building the Tour Overview - Part 1

--- controllers/viewController.js
+++ controllers/viewController.js
@@ -1,8 +1,18 @@
-exports.getOverview = (req, res) => {
+const Tour = require('../models/tourModel');
+const catchAsync = require('../utils/catchAsync');
+
+exports.getOverview = catchAsync(async (req, res, next) => {
+  // 1) Get tour data from collection
+  const tours = await Tour.find();
+  // 2) build templates
+
+  // 3)
+
   res.status(200).render('overview', {
     title: 'All Tours',
+    tours, // means (tours: tours) are the same as tours in in ES6
   });
-};
+});
 
 exports.getTour = (req, res) => {
   res.status(200).render('tour', {
