340db68f9d0f3ffa4ce276bd7e73ca9befb17341
Sintayehu <sentflix@gmail.com>
2025-08-07 12:24:22 +0300

    015 Building the Login Screen

--- routes/viewRoutes.js
+++ routes/viewRoutes.js
@@ -5,5 +5,6 @@ const Router = express.Router();
 
 Router.get('/', viewsController.getOverview);
 Router.get('/tour/:slug', viewsController.getTour);
+Router.get('/login', viewsController.getLoginForm);
 
 module.exports = Router;
