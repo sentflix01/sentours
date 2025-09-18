5bb81df9e2ac68ba7905273997378fe76e5be666
Sintayehu Mulugeta <sentflix@gmail.com>
2025-09-18 22:52:52 +0300

    216 - Finishing the Bookings API

--- routes/bookingRoutes.js
+++ routes/bookingRoutes.js
@@ -4,10 +4,19 @@ const authController = require('../controllers/authController');
 
 const Router = express.Router();
 
-Router.get(
-  '/checkout-session/:tourId',
-  authController.protect,
-  bookingController.getCheckoutSession,
-);
+Router.use(authController.protect);
+
+Router.get('/checkout-session/:tourId', bookingController.getCheckoutSession);
+
+Router.use(authController.restrictTo('admin', 'lead-guide'));
+
+Router.route('/')
+  .get(bookingController.getAllBookings)
+  .post(bookingController.createBooking);
+
+Router.route('/:id')
+  .get(bookingController.getBooking)
+  .patch(bookingController.updateBooking)
+  .delete(bookingController.deleteBooking);
 
 module.exports = Router;
