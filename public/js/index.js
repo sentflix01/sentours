55da9940152b17985b52d27b8a757626b25b25ec
Sintayehu Mulugeta <sentflix@gmail.com>
2025-10-24 19:44:09 +0300

    add signup index.js

--- public/js/index.js
+++ public/js/index.js
@@ -7,6 +7,7 @@ import { bookTour } from './stripe';
 import { showAlert } from './alerts';
 //DOM ELEMENT
 const leaflet = document.getElementById('map');
+const signupForm = document.querySelector('.form--signup');
 const loginForm = document.querySelector('.form--login');
 const logOutBtn = document.querySelector('.nav__el--logout');
 const userDataForm = document.querySelector('.form-user-data');
@@ -17,6 +18,17 @@ if (leaflet) {
   const locations = JSON.parse(leaflet.dataset.locations);
   displayMap(locations);
 }
+
+if (signupForm)
+  signupForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+    // value
+    const name = document.getElementById('name').value;
+    const email = document.getElementById('email').value;
+    const password = document.getElementById('password').value;
+    const passwordConfirm = document.getElementById('passwordConfirm').value;
+    signup(name, email, password, passwordConfirm);
+  });
 
 if (loginForm)
   loginForm.addEventListener('submit', (e) => {
